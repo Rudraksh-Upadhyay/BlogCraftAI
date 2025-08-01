56fd20726782f59d0d2ee565365e160b959ef04e
RuDrAkash <kashishupadhyay145@gmail.com>
2025-08-01 23:47:18 +0530

    commmit 8

--- app.py
+++ app.py
@@ -1,23 +1,46 @@
 import streamlit as st 
-import google.generativeai as genai
+# import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 
 
+import requests
+
 load_dotenv()
 
-try:
-    api_key = st.secrets["GOOGLE_API_KEY"]
-except KeyError:
-    api_key = os.getenv("GOOGLE_API_KEY")
-    
-if not api_key:
-    st.error("Google API Key not found. Please set it as a secret in Streamlit Cloud (GOOGLE_API_KEY) or in your local .env file.")
-    st.stop()
+EURON_API_KEY = os.getenv("EURON_API_KEY")
 
-genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+def generate_completion(content):
+    url = "https://api.euron.one/api/v1/euri/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {EURON_API_KEY}"
+    }
+    payload = {
+        "messages": [
+            {
+                "role": "user",
+                "content": content
+            }
+        ],
+        "model": "gpt-4.1-nano",
+        "max_tokens": 1000,
+        "temperature": 0.7
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    data = response.json()
+    # print(data)
+    return data
+
+# generate_completion()
+# df = load_dotenv()
+# load_dotenv()
+
+# genai.configure(api_key= os.getenv("GOOGLE_API_KEY"))
+
+# model = genai.GenerativeModel('gemini-1.5-flash')
 
 st.set_page_config(layout="wide")
 
@@ -48,12 +71,13 @@ with st.sidebar:
                 prompt = f"Generate a comprehensive, engaging blog post relevant to the given title '{blog_title}' including the keywords '{keywords}' and with a target length of {num_of_words} words. This should be suitable for an online audience. Ensure the content is engaging, informative, original, unique, humanized and maintains a consistent tone throughout"
                 
                 try:
-                    response = model.generate_content(prompt)
-                    
+                    # response = model.generate_content(prompt)
+                    response = generate_completion(prompt)
+                    generated_text  = response.get("choices", [{}])[0].get("message", {}).get("content", "")
                     #display kaise krenge
                     # st.subheader("Your Generated Blog Post:")
                     # st.write(response.text)
-                    st.session_state['generated_blog'] = response.text
+                    st.session_state['generated_blog'] = generated_text
                 
                 except Exception as e:
                     # st.error(f"An error occurred: {e}")
